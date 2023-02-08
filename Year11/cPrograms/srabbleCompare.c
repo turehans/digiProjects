64acfbda81775f9911946145dcf43c6db2616c40
turehans <96639027+turehans@users.noreply.github.com>
2023-02-09 10:42:22 +1300

    Created Repo and started scrabbleCompare.c

---
+++ Year11/cPrograms/srabbleCompare.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <math.h>
+#include <stdlib.h>
+#include <string.h>
+#include <strings.h>
+
+//declare functions
+void addLetters();
+
+//create struct scrabbleLetters
+struct scrabbleLetters {
+  char  Letters;
+  int Points;
+};
+
+//create an array of the number of points for each letter
+struct scrabbleLetters scrabbleLetters[26];
+
+int main(){
+	//call function that adds letters to array
+	addLetters();
+	for (int i = 0; i < 26; i++){
+		printf("Letter %i: %c",i ,scrabbleLetters[i].Letters);
+	}
+} 
+
+void addLetters(){
+	for (int i = 0; i < 26; i++){
+		scrabbleLetters[i].Letters = 'A' + i;
+	}
+}

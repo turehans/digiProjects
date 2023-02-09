d64d7a22a608a26f535add555530ba6736643056
turehans <96639027+turehans@users.noreply.github.com>
2023-02-09 16:36:24 +1300

    Just Did some stuff

--- Year11/cPrograms/scrabbleCompare.c
+++ Year11/cPrograms/scrabbleCompare.c
@@ -7,6 +7,7 @@
 
 //declare functions
 void addLetters();
+void addPoints();
 
 //create struct scrabbleLetters
 struct scrabbleLetters {
@@ -20,9 +21,7 @@ struct scrabbleLetters scrabbleLetters[26];
 int main(){
 	//call function that adds letters to array
 	addLetters();
-	for (int i = 0; i < 26; i++){
-		printf("Letter %i: %c\n",i ,scrabbleLetters[i].Letters);
-	}
+	//call function that adds corresponding points
 } 
 
 void addLetters(){
@@ -30,3 +29,38 @@ void addLetters(){
 		scrabbleLetters[i].Letters = 'A' + i;
 	}
 }
+
+void addPoints(){
+	for (int i = 0; i < 26; i++){
+		if (scrabbleLetters[i].Letters == 'A' || scrabbleLetters[i].Letters == 'E' || scrabbleLetters[i].Letters == 'I' || scrabbleLetters[i].Letters == 'O' || scrabbleLetters[i].Letters == 'U' || scrabbleLetters[i].Letters == 'L' || scrabbleLetters[i].Letters == 'N' || scrabbleLetters[i].Letters == 'S'|| scrabbleLetters[i].Letters == 'T' || scrabbleLetters[i].Letters == 'R')
+		{
+			scrabbleLetters[i].Points = 1;
+		} 
+		else if (scrabbleLetters[i].Letters == 'D' || scrabbleLetters[i].Letters == 'G')
+		{
+			scrabbleLetters[i].Points = 2;
+		} 
+		else if (scrabbleLetters[i].Letters == 'B' || scrabbleLetters[i].Letters == 'C' || scrabbleLetters[i].Letters == 'M' || scrabbleLetters[i].Letters == 'P')
+		{
+			scrabbleLetters[i].Points = 3;
+		}
+		else if (scrabbleLetters[i].Letters == 'F' || scrabbleLetters[i].Letters == 'H' || scrabbleLetters[i].Letters == 'V' || scrabbleLetters[i].Letters == 'W' || scrabbleLetters[i].Letters == 'Y')
+		{
+			scrabbleLetters[i].Points = 4;
+		}
+		else if (scrabbleLetters[i].Letters == 'K')
+		{
+			scrabbleLetters[i].Points = 5;
+		}
+		else if (scrabbleLetters[i].Letters == 'J' || scrabbleLetters[i].Letters == 'X')
+		{
+			scrabbleLetters[i].Points = 8;
+		}
+		else if (scrabbleLetters[i].Letters == 'Q' || scrabbleLetters[i].Letters == 'Z')
+		{
+			scrabbleLetters[i].Points = 10;
+		}
+		
+		
+				
+	}
